bdf11af0fa79667cb7fbf497ae4fbf0bfd32d654
Dénes Fehér <denes.m.feher@gmail.com>
2022-04-14 17:31:23 +0200

    Fix errors

--- SigStatCompare/MauiProgram.cs
+++ SigStatCompare/MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿namespace SigStatCompare;
+﻿using Microsoft.Maui.Controls.Compatibility.Hosting;
+
+namespace SigStatCompare;
 
 public static class MauiProgram
 {
@@ -7,6 +9,7 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
+			.UseMauiCompatibility()
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
