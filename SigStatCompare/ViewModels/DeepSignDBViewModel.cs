5fc8d49eb21c566ca55c17b40c70cd0e4ab76bbe
Dénes Fehér <denes.m.feher@gmail.com>
2022-09-17 15:14:47 +0200

    Add DeepSignDBViewModel

---
+++ SigStatCompare/ViewModels/DeepSignDBViewModel.cs
@@ -0,0 +1,83 @@
+﻿namespace SigStatCompare.ViewModels;
+
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using SigStat.Common;
+
+public class DeepSignDBViewModel : INotifyPropertyChanged
+{
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    private ObservableCollection<Signer> signers;
+    public ObservableCollection<Signer> Signers
+    {
+        get { return signers; }
+        set
+        {
+            if (value != signers)
+            {
+                signers = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    private int signatureCount;
+    public int SignatureCount
+    {
+        get { return signatureCount; }
+        set
+        {
+            if (value != signatureCount)
+            {
+                signatureCount = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    private object filePickerLock = new object();
+    private bool isFilePickerOpen = false;
+
+    public Command LoadCommand => new(async () =>
+    {
+        lock (filePickerLock)
+        {
+            if (isFilePickerOpen) return;
+            isFilePickerOpen = true;
+        }
+
+        var databaseDir = Environment.GetEnvironmentVariable("SigStatDB");
+
+        FileResult fileResult = await FilePicker.PickAsync();
+
+        if (fileResult is not null)
+        {
+            Console.WriteLine(fileResult.FullPath);
+
+            var loader = new SVC2021.Svc2021Loader(fileResult.FullPath, false);
+            Signers = await Task.Run(() => new ObservableCollection<Signer>(loader.EnumerateSigners().OrderBy(s => s.ID)));
+            SignatureCount = await Task.Run(() =>
+            {
+                int count = 0;
+                foreach (var signer in signers)
+                {
+                    count += signer.Signatures.Count;
+                }
+                return count;
+            });
+        }
+
+        isFilePickerOpen = false;
+    });
+
+    public DeepSignDBViewModel()
+    {
+
+    }
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+}
--- SigStatCompare/Views/DeepSignDBPage.xaml.cs
+++ SigStatCompare/Views/DeepSignDBPage.xaml.cs
@@ -1,9 +1,17 @@
+using SigStatCompare.ViewModels;
+
 namespace SigStatCompare.Views;
 
 public partial class DeepSignDBPage : ContentPage
 {
-	public DeepSignDBPage()
-	{
-		InitializeComponent();
-	}
+    public void LoadDeepSignDB(object sender, EventArgs e)
+    {
+        var viewModel = BindingContext as DeepSignDBViewModel;
+        viewModel.LoadCommand.Execute(null);
+    }
+
+    public DeepSignDBPage()
+    {
+        InitializeComponent();
+    }
 }
