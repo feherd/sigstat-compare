d2606c22ef2ad50d835a565c7d07170f4e5cecd1
Dénes Fehér <denes.m.feher@gmail.com>
2022-10-02 12:28:55 +0100

    Add DataSetParametersViewModel

---
+++ SigStatCompare/ViewModels/DataSetParametersViewModel.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using SigStatCompare.Views;
+
+namespace SigStatCompare.ViewModels;
+
+public partial class DataSetParametersViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private string name;
+
+    [ObservableProperty]
+    private int signerCount;
+
+    [ObservableProperty]
+    private int genuinePairCountPerSigner;
+
+    [ObservableProperty]
+    private int skilledForgeryCountPerSigner;
+
+    [ObservableProperty]
+    private int randomForgeryCountPerSigner;
+}
