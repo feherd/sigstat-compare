ed111df4942ac1a9cf8024b4544067bc7095c4d2
Dénes Fehér <denes.m.feher@gmail.com>
2022-10-06 21:14:28 +0200

    Add IntToDoubleConverterTransformation

---
+++ SigStatCompare/Models/IntToDoubleConverterTransformation.cs
@@ -0,0 +1,28 @@
+using SigStat.Common;
+using SigStat.Common.Pipeline;
+
+namespace SigStatCompare.Models;
+
+class IntToDoubleConverterTransformation : PipelineBase, ITransformation
+{
+    [Input]
+    FeatureDescriptor<List<int>> IntInput { get; set; }
+
+    [Output]
+    FeatureDescriptor<List<double>> DoubleOutput { get; set; }
+
+    public IntToDoubleConverterTransformation(FeatureDescriptor<List<int>> intInput, FeatureDescriptor<List<double>> doubleOutput)
+    {
+        IntInput = intInput;
+        DoubleOutput = doubleOutput;
+    }
+
+    public void Transform(Signature signature)
+    {
+        signature.SetFeature(DoubleOutput,
+            signature.GetFeature(IntInput)
+                .Select(i => (double)i)
+                .ToList()
+        );
+    }
+}
