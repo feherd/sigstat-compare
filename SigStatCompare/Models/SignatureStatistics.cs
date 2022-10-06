b45dd2f4bdd3c68a4d16de88b25f3383b2597f03
Dénes Fehér <denes.m.feher@gmail.com>
2022-10-06 18:55:17 +0200

    Add SignatureStatistics

---
+++ SigStatCompare/Models/SignatureStatistics.cs
@@ -0,0 +1,10 @@
+namespace SigStatCompare.Models;
+
+struct SignatureStatistics
+{
+    public double stdevX;
+    public double stdevY;
+    public double stdevP;
+    public int count;
+    public int duration;
+}
